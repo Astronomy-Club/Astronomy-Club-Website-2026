b820cef43627deab1aded2e8234e8b1af41b01a5
achyantsh06 <achyantsh06@gmail.com>
2026-02-06 15:42:00 +0530

    added activity page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+
+
+// If you have a ParticleEffect component, import it here
+// import ParticleEffect from '../components/ParticleEffect';
+
+const Home: React.FC = () => {
+    return (
+    <div className="min-h-screen bg-black">
+      {/* Content will be added later */}
+    </div>
+  );
+};
+
+export default Home;
