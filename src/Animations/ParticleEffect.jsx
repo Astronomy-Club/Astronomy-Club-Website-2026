28fa4973216c06bac438fe5969b7d5f91bea14e6
Agni005 <agni.jhariya.che24@itbhu.ac.in>
2026-02-08 09:56:05 +0530

    Fixed the home page and footer, added bg animation, and increased logo size in navbar

---
+++ src/Animations/ParticleEffect.jsx
@@ -0,0 +1,81 @@
+import { useCallback } from "react";
+import Particles from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+
+
+const ParticleEffect = () => {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+    return (
+        <>
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    "fullScreen": {
+                        "enable": false,
+                        "zIndex": -1,
+
+                    },
+                    fpsLimit: 120,
+
+                    interactivity: {
+                        events: {
+                            resize: true,
+                        },
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 2,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 500,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 0.7, max: 3 },
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
+        </>
+    )
+}
+
+export default ParticleEffect
